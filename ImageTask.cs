8763c52882e0db1197568b2697b5820a110d5282
Stevek <vegh@spsejecna.cz>
2025-11-21 09:53:16 +0100

    Added ImageTaskQueue and ImageTask

---
+++ ImageTask.cs
@@ -0,0 +1,17 @@
+﻿namespace MassImageEditor;
+
+/// <summary>
+/// Represents a task for processing an image file.
+/// This class encapsulates the file path for the input image and the path where the output will be saved.
+/// </summary>
+public sealed class ImageTask
+{
+    public string FilePath { get; set; }
+    public string OutputPath { get; set; }
+
+    public ImageTask(string filePath, string outputPath)
+    {
+        FilePath = filePath;
+        OutputPath = outputPath;
+    }
+}
